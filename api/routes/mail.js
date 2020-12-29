c84ee724f0d4a225cc8c6ccdc59f6c7eb4572a65
Rock well <momorockwell@gmail.com>
2020-12-29 19:43:48 +0100

    update env

--- api/routes/mail.js
+++ api/routes/mail.js
@@ -4,6 +4,7 @@ const router = express.Router();
 const mailController = require('../controllers/mails');
 
 
+
 router.post('/create', mailController.create);
 router.delete('/:id', mailController.delete);
 router.patch('/:id', mailController.update);
