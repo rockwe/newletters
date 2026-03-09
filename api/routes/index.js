895a2a15396a4b62517e518ed79ce5fb29559b16
Bar2Choco <chokl@MSI.(none)>
2026-03-09 11:33:16 +0100

    update

--- api/routes/index.js
+++ api/routes/index.js
@@ -5,6 +5,6 @@ const contactRoutes = require('./contact');
 module.exports = (app) => {
     app.use('/api/v1/user', mailRoutes);
     app.use('/api/v1/contact', contactRoutes);
-
+    //app.use("Andy") 
 
 };
