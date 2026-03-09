ddc873215cf00b4835d847118049eee46235d829
mariam <siskomiraaa@gmail.com>
2026-03-09 11:54:04 +0100

    fichier modifié par Maryam

--- api/routes/index.js
+++ api/routes/index.js
@@ -5,6 +5,7 @@ const contactRoutes = require('./contact');
 module.exports = (app) => {
     app.use('/api/v1/user', mailRoutes);
     // app use "Anderson"
+// app.use "Maryam"
     app.use('/api/v1/contact', contactRoutes);
     //app.Use("Idrissa")
     
