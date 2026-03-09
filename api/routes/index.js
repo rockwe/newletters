32f636dff51fe108f0913bef2ab8b8c853cbb0c1
rockwe <syllasouleymane357@gmail.com>
2026-03-09 12:39:43 +0100

    Modification2

--- api/routes/index.js
+++ api/routes/index.js
@@ -6,12 +6,9 @@ module.exports = (app) => {
     app.use('/api/v1/user', mailRoutes);
     // app use "Anderson"
     app.use('/api/v1/contact', contactRoutes);
-<<<<<<< HEAD
     //app.Use("Idrissa")
     
-=======
     //app.Use("Souleymane")
->>>>>>> 734dac3 (modification index.js)
 
     // app.use("Audrey")
 };
